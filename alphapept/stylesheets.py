154f2eb31db874f0e621846e5648767d56b5b4f0
Maximilian Strauss <straussmaximilian@gmail.com>
2020-05-04 20:07:45 +0200

    FIX added stylesheets

---
+++ alphapept/stylesheets.py
@@ -0,0 +1,109 @@
+
+
+big_font = (
+    "    font-family: Roboto Thin;\n"
+    "    color: white;\n"
+    "    font-size: 22pt; \n"
+    )
+
+logo_font = (
+    "    font-family: Roboto Thin;\n"
+    "    color: white;\n"
+    "    font-size: 28pt; \n"
+    )
+
+version_font = (
+    "    font-family: Roboto;\n"
+    "    color:rgb(191, 191, 191);\n"
+    "    font-size: 10pt; \n"
+    )
+
+busy_style = (
+    "QProgressBar{\n"
+    "    border: 0px solid white;\n"
+    "    border-radius: 0px;\n"
+    "    text-align: center;\n"
+    "	color:rgb(255, 255, 255);\n"
+    "}\n"
+    "QProgressBar::chunk {\n"
+    "    background-color:rgb(191, 191, 191);\n"
+    "    width: 10px;\n"
+    "    margin: 0px;\n"
+    "	border-radius: 0px;\n"
+    "}\n"
+)
+
+progress_style = (
+    "QProgressBar{\n"
+    "    border: 0px solid white;\n"
+    "    border-radius: 0px;\n"
+    "    text-align: center;\n"
+    "	color:rgb(255, 255, 255);\n"
+    "}\n"
+    "QProgressBar::chunk {\n"
+    "    background-color:rgb(109, 236, 185);\n"
+    "    width: 10px;\n"
+    "    margin: 0px;\n"
+    "	border-radius: 0px;\n"
+    "}\n"
+)
+
+progress_style_1 = (
+    "QProgressBar{\n"
+    "    border: 0px solid white;\n"
+    "    border-radius: 0px;\n"
+    "    text-align: center;\n"
+    "	color:rgb(255, 255, 255);\n"
+    "}\n"
+    "QProgressBar::chunk {\n"
+    "    background-color:rgb(63, 197, 240);\n"
+    "    width: 10px;\n"
+    "    margin: 0px;\n"
+    "	border-radius: 0px;\n"
+    "}\n"
+)
+
+progress_style_2 = (
+    "QProgressBar{\n"
+    "    border: 0px solid white;\n"
+    "    border-radius: 0px;\n"
+    "    text-align: center;\n"
+    "	color:rgb(255, 255, 255);\n"
+    "}\n"
+    "QProgressBar::chunk {\n"
+    "    background-color:rgb(66, 222, 225);\n"
+    "    width: 10px;\n"
+    "    margin: 0px;\n"
+    "	border-radius: 0px;\n"
+    "}\n"
+)
+
+progress_style_3 = (
+    "QProgressBar{\n"
+    "    border: 0px solid white;\n"
+    "    border-radius: 0px;\n"
+    "    text-align: center;\n"
+    "	color:rgb(255, 255, 255);\n"
+    "}\n"
+    "QProgressBar::chunk {\n"
+    "    background-color:rgb(109, 236, 185);\n"
+    "    width: 10px;\n"
+    "    margin: 0px;\n"
+    "	border-radius: 0px;\n"
+    "}\n"
+)
+
+progress_style_4 = (
+    "QProgressBar{\n"
+    "    border: 0px solid white;\n"
+    "    border-radius: 0px;\n"
+    "    text-align: center;\n"
+    "	 color:rgb(255, 255, 255);\n"
+    "}\n"
+    "QProgressBar::chunk {\n"
+    "    background-color:rgb(238, 245, 178);\n"
+    "    width: 10px;\n"
+    "    margin: 0px;\n"
+    "	border-radius: 0px;\n"
+    "}\n"
+)
