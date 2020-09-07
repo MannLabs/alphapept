614b47dbadc04cdc9a26d7abd70ac2dd57ba631b
Sander Willems <swillems89@hotmail.com>
2020-09-07 16:38:57 +0200

    update of __version__ with settings.ini

--- alphapept/__version__.py
+++ alphapept/__version__.py
@@ -1,28 +1,34 @@
-VERSION_NO = "0.2.8-dev0"
-COPYRIGHT = "2020 Mann Labs"
-URL = "https://github.com/MannLabs/alphapept"
-URL_DOCUMENTATION = "https://mannlabs.github.io/alphapept/"
-URL_ISSUE = "https://github.com/MannLabs/alphapept/issues"
-URL_CONTRIBUTE = "https://github.com/MannLabs/alphapept/blob/master/CONTRIBUTING.md"
-LIB_NAME = "alphapept"
-USER = "mannlabs"
-DESCRIPTION = "A modular, python-based framework for mass spectrometry."
-KEYWORDS = "mass spec, python, numba"
-AUTHOR = "Maximilian T. Strauss"
-AUTHOR_EMAIL = "straussmaximilian@gmail.com"
-BRANCH = "master"
-MIN_PYTHON = "3.6"
-MAX_PYTHON = "4"
-AUDIENCE = "Developers"
-LANGUAGE = "English"
-CUSTOM_SIDEBAR = "False"
-LICENSE = "MIT"
-STATUS = "2"
-NBS_PATH = "nbs"
-DOC_PATH = "docs"
-DOC_HOST = "https://mannlabs.github.io"
-DOC_BASEURL = "/alphapept/"
-GIT_URL = "https://github.com/mannlabs/alphapept/tree/master/"
-LIB_PATH = "alphapept"
-TITLE = "alphapept"
-HOST = "github"
+from configparser import ConfigParser
+
+config = ConfigParser(delimiters=['='])
+config.read('settings.ini')
+cfg = config['DEFAULT']
+
+LIB_NAME = cfg["lib_name"]
+USER = cfg["user"]
+DESCRIPTION = cfg["description"]
+KEYWORDS = cfg["keywords"]
+AUTHOR = cfg["author"]
+AUTHOR_EMAIL = cfg["author_email"]
+COPYRIGHT = cfg["copyright"]
+BRANCH = cfg["branch"]
+VERSION_NO = cfg["version"]
+MIN_PYTHON = cfg["min_python"]
+MAX_PYTHON = cfg["max_python"]
+AUDIENCE = cfg["audience"]
+LANGUAGE = cfg["language"]
+CUSTOM_SIDEBAR = cfg["custom_sidebar"]
+LICENSE = cfg["license"]
+STATUS = cfg["status"]
+NBS_PATH = cfg["nbs_path"]
+DOC_PATH = cfg["doc_path"]
+DOC_HOST = cfg["doc_host"]
+DOC_BASEURL = cfg["doc_baseurl"]
+GIT_URL = cfg["git_url"]
+LIB_PATH = cfg["lib_path"]
+TITLE = cfg["title"]
+HOST = cfg["host"]
+URL = cfg["url"]
+URL_DOCUMENTATION = cfg["url_documentation"]
+URL_ISSUE = cfg["url_issue"]
+URL_CONTRIBUTE = cfg["url_contribute"]
