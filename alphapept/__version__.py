599a627fa4e5c319a6b8f25c47e0ad01625f1018
Sander Willems <swillems89@hotmail.com>
2020-09-08 08:14:19 +0200

    FIX Temporary hardcopy of settings.ini to __version__.py

--- alphapept/__version__.py
+++ alphapept/__version__.py
@@ -1,41 +1,41 @@
-from configparser import ConfigParser
-import os
+# from configparser import ConfigParser
+# import os
+#
+# config = ConfigParser(delimiters=['='])
+# config.read(
+#     os.path.join(
+#         os.path.dirname(__file__),
+#         '..',
+#         'settings.ini'
+#     )
+# )
+# cfg = config['DEFAULT']
 
-config = ConfigParser(delimiters=['='])
-config.read(
-    os.path.join(
-        os.path.dirname(__file__),
-        '..',
-        'settings.ini'
-    )
-)
-cfg = config['DEFAULT']
-
-LIB_NAME = cfg["lib_name"]
-USER = cfg["user"]
-DESCRIPTION = cfg["description"]
-KEYWORDS = cfg["keywords"]
-AUTHOR = cfg["author"]
-AUTHOR_EMAIL = cfg["author_email"]
-COPYRIGHT = cfg["copyright"]
-BRANCH = cfg["branch"]
-VERSION_NO = cfg["version"]
-MIN_PYTHON = cfg["min_python"]
-MAX_PYTHON = cfg["max_python"]
-AUDIENCE = cfg["audience"]
-LANGUAGE = cfg["language"]
-CUSTOM_SIDEBAR = cfg["custom_sidebar"]
-LICENSE = cfg["license"]
-STATUS = cfg["status"]
-NBS_PATH = cfg["nbs_path"]
-DOC_PATH = cfg["doc_path"]
-DOC_HOST = cfg["doc_host"]
-DOC_BASEURL = cfg["doc_baseurl"]
-GIT_URL = cfg["git_url"]
-LIB_PATH = cfg["lib_path"]
-TITLE = cfg["title"]
-HOST = cfg["host"]
-URL = cfg["url"]
-URL_DOCUMENTATION = cfg["url_documentation"]
-URL_ISSUE = cfg["url_issue"]
-URL_CONTRIBUTE = cfg["url_contribute"]
+LIB_NAME = "alphapept"
+USER = "mannlabs"
+DESCRIPTION = "A modular, python-based framework for mass spectrometry."
+KEYWORDS = "MS, mass spectrometry, python, numba"
+AUTHOR = "Maximilian T. Strauss"
+AUTHOR_EMAIL = "straussmaximilian@gmail.com"
+COPYRIGHT = "Mann Labs"
+BRANCH = "master"
+VERSION_NO = "0.2.8-dev0"
+MIN_PYTHON = "3.6"
+MAX_PYTHON = "4"
+AUDIENCE = "Developers"
+LANGUAGE = "English"
+CUSTOM_SIDEBAR = "False"
+LICENSE = "MIT"
+STATUS = "2"
+NBS_PATH = "nbs"
+DOC_PATH = "docs"
+DOC_HOST = "https://mannlabs.github.io"
+DOC_BASEURL = "/alphapept/"
+GIT_URL = "https://github.com/mannlabs/alphapept/tree/master/"
+LIB_PATH = "alphapept"
+TITLE = "alphapept"
+HOST = "github"
+URL = "https://github.com/MannLabs/alphapept"
+URL_DOCUMENTATION = "https://mannlabs.github.io/alphapept/"
+URL_ISSUE = "https://github.com/MannLabs/alphapept/issues"
+URL_CONTRIBUTE = "https://github.com/MannLabs/alphapept/blob/master/CONTRIBUTING.md"
