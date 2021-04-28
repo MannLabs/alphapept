9ad2af23b1e911f7d7abc6fb1914d7ae3489611c
Zeng, Wen-Feng <wzeng@biochem.mpg.de>
2021-04-28 16:26:06 +0200

    pyrawfilerader GetCreationDate ToString

--- alphapept/pyrawfilereader.py
+++ alphapept/pyrawfilereader.py
@@ -193,7 +193,7 @@ class RawFileReader(object):
         """Returns the file creation date in DATE format."""
         # https://msdn.microsoft.com/en-us/library/82ab7w69.aspx
         # The DATE type is implemented using an 8-byte floating-point number
-        return time.gmtime(self.source.CreationDate.ToOADate())
+        return self.source.CreationDate.ToString('o')
 
     def IsError(self):
         """Returns the error state flag of the raw file. A return value of TRUE indicates that an error has
