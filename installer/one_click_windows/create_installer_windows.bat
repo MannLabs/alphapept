4eb3deeca2082714d0347ff446e03bf4f3e48351
Maximilian Strauss <straussmaximilian@gmail.com>
2021-05-05 23:24:37 +0200

    FIX

--- installer/one_click_windows/create_installer_windows.bat
+++ installer/one_click_windows/create_installer_windows.bat
@@ -11,9 +11,9 @@ call DEL /F/Q/S dist > NUL
 call RMDIR /Q/S build
 call RMDIR /Q/S dist
 call python setup.py sdist bdist_wheel
-call pip install dist\alphapept-0.3.20.dev0-py3-none-any.whl
+call pip install dist/alphapept-0.3.20.dev0-py3-none-any.whl
 call pip install pyinstaller==4.2
-call cd installer dist\one_click_windows
+call cd installer dist/one_click_windows
 call pyinstaller ../alphapept.spec -y
 call conda deactivate
 call "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" alphapept_innoinstaller.iss
