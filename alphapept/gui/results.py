2eb6c574218a4315fe196570b5a146fd5d00a771
Maximilian Strauss <straussmaximilian@gmail.com>
2021-05-07 17:14:24 +0200

    FIX no file present

--- alphapept/gui/results.py
+++ alphapept/gui/results.py
@@ -82,6 +82,8 @@ def results():
 
     #TOdo: include previously processed output files..
 
+    file = ''
+
     selection = st.selectbox('File selection', ('Previous results', 'Enter file'))
 
     if selection == 'Previous results':
@@ -111,7 +113,7 @@ def results():
     elif selection == 'Enter file':
         file = st.text_input("Enter path to hdf file.", os.getcwd())
     else:
-        file = ''
+        pass
 
     if file is None:
         file = ''
