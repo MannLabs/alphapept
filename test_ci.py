3af2414e267d18b46d92ad380c25a81a899ffd8f
Maximilian Strauss <straussmaximilian@gmail.com>
2021-05-29 13:42:30 +0200

    FIX typo

--- test_ci.py
+++ test_ci.py
@@ -146,7 +146,7 @@ class TestRun():
 
         self.settings = load_settings_as_template(DEFAULT_SETTINGS_PATH)
         self.settings['experiment']['file_paths'] =  [os.path.join(TEST_DIR, _) for _ in self.file_paths]
-        self.settings['experiment']['fasta_paths'] = [s.path.join(TEST_DIR, _) for _ in self.fasta_paths]
+        self.settings['experiment']['fasta_paths'] = [os.path.join(TEST_DIR, _) for _ in self.fasta_paths]
 
     def run(self, password=None):
         if self.new_files:
