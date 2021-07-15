1183b6bb60cf0977c3b3eda2f2cb30ecad791da4
Zeng, Wen-Feng <wzeng@biochem.mpg.de>
2021-07-15 10:01:15 +0200

    DOCUMENT revised chem

--- alphapept/chem.py
+++ alphapept/chem.py
@@ -20,6 +20,10 @@ spec = [
 class IsotopeDistribution:
     """
     Class to calculate Isotope Distributions.
+    Members:
+        self.m0: the mono-isotopic mass
+        self.dm: number of isotopes
+        self.intensities: isotope patterns
     """
     def __init__(self):
 
@@ -27,17 +31,34 @@ class IsotopeDistribution:
         self.dm = 1
         self.intensities = np.array([1.0])
 
-    def add(self, x):
+    def add(self, x)->None:
+        """
+        Convolute self with the other IsotopeDistribution
+        Args:
+            x (IsotopeDistribution): the other object to be convoluted with self.
+        Returns:
+            None. self.m0, self.dm and self.intensities will be updated inplace.
+        """
         self.m0, self.dm, self.intensities = fast_add(
             self.m0, self.dm, self.intensities, x.m0, x.dm, x.intensities
         )
 
-    def add_from_dict(self, x):
+    def add_from_dict(self, x)->None:
+        """
+        Convolute self with a np.ndarray
+        Args:
+            x (np.ndarray:float): the np.ndarray to be convoluted. x[0] is the m0, x[1] is the dm, and x[2:] is the intensity pattern
+        Returns:
+            None. self.m0, self.dm and self.intensities will be updated inplace.
+        """
         self.m0, self.dm, self.intensities = fast_add(
             self.m0, self.dm, self.intensities, x[0], np.int(x[1]), x[2:]
         )
 
-    def copy(self):
+    def copy(self)->IsotopeDistribution:
+        """
+        Copy this instance
+        """
         i = IsotopeDistribution()
         i.m0 = self.m0
         i.dm = self.dm
@@ -45,7 +66,14 @@ class IsotopeDistribution:
 
         return i
 
-    def mult(self, n):
+    def mult(self, n)->IsotopeDistribution:
+        """
+        Convolute this instance for n times. Using "binary search"-like strategy to accelerate the convolution.
+        Args:
+            n (int): how many times to convolute this instance
+        Returns:
+            IsotopeDistribution:
+        """
         binary = numba_bin(n)
 
         if n == 1:
@@ -66,7 +94,17 @@ class IsotopeDistribution:
 @njit
 def fast_add(m0, dm0, int0, m1, dm1, int1, PRUNE_LEVEL=0.000001):
     """
-    Function for fast estimation of masses
+    Convolute two isotope distributions
+    Args:
+        m0 (float): the mono-isotopic mass of the first isotope distribution.
+        dm0 (int): the number of isotope intensities in the first isotope distribution.
+        int0 (np.ndarray:float): the intensity pattern of the first isotope distribution.
+        m1 (float): the mono-isotopic mass of the second isotope distribution.
+        dm1 (int): the number of isotope intensities in the second isotope distribution.
+        int1 (np.ndarray:float): the intensity pattern of the second isotope distribution.
+        PRUNE_LEVEL: relative intensity below this value will be discarded.
+    Returns:
+        tuple of (float, int, np.ndarray): the updated isotope distributions
     """
 
     m0 += m1
@@ -86,9 +124,9 @@ def fast_add(m0, dm0, int0, m1, dm1, int1, PRUNE_LEVEL=0.000001):
     return m0, dm0, int0
 
 @njit
-def numba_bin(decimal):
+def numba_bin(decimal:int)->list:
     """
-    Numba compatibale function to convert decimal to binary as list
+    Numba compatibale function to convert decimal to binary as a list
     """
 
     binary = []
@@ -101,9 +139,17 @@ def numba_bin(decimal):
     return binary
 
 @njit
-def dict_to_dist(counted_AA, isotopes):
+def dict_to_dist(
+    counted_AA:Dict,
+    isotopes:Dict)->IsotopeDistribution:
     """
     Function to convert a dictionary with numbers of amino acids to a isotope distribution.
+    Args:
+        counted_AA (numba.typed.Dict): key is the chemical element and value is the element number.
+        isotopes (numba.typed.Dict): key is the chemical element and the value is the alphapept.constants.isotopes
+
+    Returns:
+        IsotopeDistribution: isotope distribution with m0 and intensites.
     """
     dist = IsotopeDistribution()
     for AA in counted_AA.keys():
@@ -125,6 +171,13 @@ from .constants import averagine_avg
 def get_average_formula(molecule_mass, averagine_aa, isotopes, sulphur=True):
     """
     Function to calculate the averagine formula for a molecule mass.
+    Args:
+        molecule_mass (float): the peptide mass to calculate the averagine.
+        averagine_aa (dict): the averagine chemical compositions. See alphapept.constants.averagine_aa.
+        isotopes (dict): the isotope patterns for each chemical element. See alphapept.constants.isotopes.
+        sulphur (bool): Mode w/o sulphur is not implemented yet.
+    Returns:
+        dict: the averagine chemical compositions for the given molecule_mass
     """
 
     if sulphur:
