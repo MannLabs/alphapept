822391e7528db11bfd0755316c3513d85558545e
Maximilian Strauss <straussmaximilian@gmail.com>
2021-09-09 15:11:39 +0200

    FIX no home dir present

--- alphapept/paths.py
+++ alphapept/paths.py
@@ -16,12 +16,12 @@ FASTA_PATH = os.path.join(HOME, ".alphapept", "fasta")
 
 PLOT_SETTINGS = os.path.join(HOME, ".alphapept", 'custom_plots.yaml')
 
-if not os.path.isfile(PLOT_SETTINGS):
-    copyfile(os.path.join(_this_directory, 'custom_plots.yaml'), PLOT_SETTINGS)
-
 PROCESS_FILE = os.path.join(QUEUE_PATH, 'process')
 FILE_WATCHER_FILE = os.path.join(QUEUE_PATH, 'file_watcher')
 
 for folder in [AP_PATH, QUEUE_PATH, PROCESSED_PATH, FAILED_PATH, FASTA_PATH]:
     if not os.path.isdir(folder):
         os.mkdir(folder)
+
+if not os.path.isfile(PLOT_SETTINGS):
+    copyfile(os.path.join(_this_directory, 'custom_plots.yaml'), PLOT_SETTINGS)
