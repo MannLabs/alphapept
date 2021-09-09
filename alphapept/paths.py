730c7b145d0364062edbe515714e20453f23725f
Maximilian Strauss <straussmaximilian@gmail.com>
2021-09-09 14:42:20 +0200

    FEAT moved custom plots to alphapept home directory

--- alphapept/paths.py
+++ alphapept/paths.py
@@ -1,9 +1,9 @@
 import os
+from shutil import copyfile
 
 _this_file = os.path.abspath(__file__)
 _this_directory = os.path.dirname(_this_file)
 
-PLOT_SETTINGS = os.path.join(_this_directory, 'custom_plots.yaml')
 DEFAULT_SETTINGS_PATH = os.path.join(_this_directory, 'default_settings.yaml')
 SETTINGS_TEMPLATE_PATH = os.path.join(_this_directory, 'settings_template.yaml')
 
@@ -13,6 +13,11 @@ QUEUE_PATH = os.path.join(HOME, ".alphapept", "queue")
 PROCESSED_PATH = os.path.join(HOME, ".alphapept", "finished")
 FAILED_PATH = os.path.join(HOME, ".alphapept", "failed")
 FASTA_PATH = os.path.join(HOME, ".alphapept", "fasta")
+
+PLOT_SETTINGS = os.path.join(HOME, ".alphapept", 'custom_plots.yaml')
+
+if not os.path.isfile(PLOT_SETTINGS):
+    copyfile(os.path.join(_this_directory, 'custom_plots.yaml'), PLOT_SETTINGS)
 
 PROCESS_FILE = os.path.join(QUEUE_PATH, 'process')
 FILE_WATCHER_FILE = os.path.join(QUEUE_PATH, 'file_watcher')
