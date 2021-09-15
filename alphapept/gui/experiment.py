a6b03353e2fa2eaee499c1f42718ec5cf2960433
Maximilian Strauss <straussmaximilian@gmail.com>
2021-09-15 12:20:49 +0200

    FIX duplicate names in finished

--- alphapept/gui/experiment.py
+++ alphapept/gui/experiment.py
@@ -8,6 +8,7 @@ from typing import Union, Tuple
 from alphapept.paths import (
     SETTINGS_TEMPLATE_PATH,
     QUEUE_PATH,
+    FINISHED_PATH,
     DEFAULT_SETTINGS_PATH,
     FASTA_PATH,
 )
@@ -140,7 +141,7 @@ def submit_experiment(recorder: dict):
 
     long_name = name + ".yaml"
     long_name_path_queue = os.path.join(QUEUE_PATH, long_name)
-    long_name_path_finished = os.path.join(QUEUE_PATH, long_name)
+    long_name_path_finished = os.path.join(FINISHED_PATH, long_name)
 
     if os.path.exists(long_name_path_queue):
         st.error(f"Name {escape_markdown(long_name)} already exists. Please rename.")
