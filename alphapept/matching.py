651be149e3f7de8b88da015ab7fe055cdd858553
Maximilian Strauss <straussmaximilian@gmail.com>
2022-01-12 19:31:32 -0800

    FIX recalibration, STREAMLIT API expection

--- alphapept/matching.py
+++ alphapept/matching.py
@@ -236,6 +236,7 @@ def align_datasets(settings:dict, callback:callable=None):
         def progress_wrapper(current, step, n_steps):
             callback((step/n_steps)+(current/n_steps))
 
+        progress_wrapper(0, 0, 2)
         cb = functools.partial(progress_wrapper, 0, 2)
     else:
         cb = None
@@ -266,6 +267,7 @@ def align_datasets(settings:dict, callback:callable=None):
         align_files(filenames, -alignment, offset_dict)
 
         if cb:
+            progress_wrapper(0, 1, 2)
             cb = functools.partial(progress_wrapper, 1, 2)
 
         deltas, weights, offset_dict = calculate_deltas(combos, calib=True, callback=cb)
