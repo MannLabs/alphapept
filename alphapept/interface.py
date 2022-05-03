7522137dc7819c54c1df94d3cf79f47efdb2be7f
Maximilian Strauss <straussmaximilian@gmail.com>
2022-05-03 21:40:51 +0200

    FEAT revised FF intensity estimation

--- alphapept/interface.py
+++ alphapept/interface.py
@@ -781,7 +781,7 @@ def quantification(
 
             else:
                 logging.info('Exporting protein intensity.')
-                protein_table = df.groupby(['protein_group','sample_group'])['ms1_int_sum'].sum().unstack()
+                protein_table = df.groupby(['protein_group','sample_group'])[field].sum().unstack()
 
             protein_table.to_hdf(
                 settings['experiment']['results_path'],
@@ -1144,7 +1144,7 @@ def get_summary(settings: dict, summary: dict) -> dict:
 
     summary['file_sizes'] = {}
 
-    fields = ['fwhm','ms1_int_sum','rt_length','rt_tail','prec_offset_raw_ppm', 'prec_offset_ppm','mobility']
+    fields = ['fwhm','ms1_int_sum_area','ms1_int_sum_apex','ms1_int_max_area','ms1_int_max_apex','rt_length','rt_tail','prec_offset_raw_ppm', 'prec_offset_ppm','mobility']
 
     file_sizes = {}
     for _ in settings['experiment']['file_paths']:
