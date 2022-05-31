cac7129b14316afc32636cf04b42411d5cca9753
Maximilian Strauss <straussmaximilian@gmail.com>
2022-05-31 13:53:17 +0200

    FEAT constants menu in GUI

---
+++ alphapept/gui/constants.py
@@ -0,0 +1,39 @@
+
+import streamlit as st
+import pandas as pd
+
+import alphapept.constants
+
+@st.cache
+def load_mods(path):
+    df = pd.read_csv(path, delimiter="\t")
+    return df
+
+
+def constants():
+    """Streamlit page to display internal constants / modifications."""
+    st.write("# Constants")
+    st.text(
+        f"Constants shows modifications and proteases that are available in AlphaPept."
+    )
+    st.write('## Modificatios')
+    st.text(
+        f"Available modifications are generated from the *.tsv file located at {alphapept.constants.modfile_path}."
+        "\nThis file can be modified to add custom modifications. After changing the modifications, restart AlphaPept."
+        "\nThe column `Monoisotopic Mass Shift (Da)` will be used."
+    )
+    mod_df = load_mods(alphapept.constants.modfile_path)
+    st.write(mod_df)
+
+    st.write('## Amino Acids')
+    st.text(
+        f"Available amino acids are generated from the *.tsv file located at {alphapept.constants.aafile_path}."
+        "\nThis file can be modified to add custom modifications. After changing the amino acids, restart AlphaPept."
+        "\nThe column `Monoisotopic Mass (Da)` will be used."
+    )
+    aa_df = load_mods(alphapept.constants.aafile_path)
+    st.write(aa_df)
+
+    st.write('## Proteases')
+    protease_dict = alphapept.constants.protease_dict
+    st.write(pd.DataFrame(protease_dict.items(), columns=['Protease', 'RegEx']))
