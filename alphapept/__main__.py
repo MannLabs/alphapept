f68c3ba9cc693a1c9fa8468533abb5e216abab7f
Maximilian Strauss <straussmaximilian@gmail.com>
2022-07-14 16:31:20 +0200

    FEAT traceback for main loop

--- alphapept/__main__.py
+++ alphapept/__main__.py
@@ -4,7 +4,7 @@
 from .__version__ import VERSION_NO
 from .__version__ import COPYRIGHT
 from .__version__ import URL
-
+import logging
 
 def main():
     try:
@@ -14,12 +14,14 @@ def main():
         print(f'\nAn exception occured running AlphaPept version {VERSION_NO}:\n')
         print('.'*52)
         print(f"\n{e}\n")
-        print('.'*52)
+        logging.exception('Traceback')
 
         if 'No module named' in str(e):
             print('\nPlease make sure to run AlphaPept in the right environment and have all required python packages installed (pip install -r requirements.txt).')
         else:
             print('\nPlease visit https://github.com/MannLabs/alphapept and report this issue or search for potential solutions. Thanks.\n')
+
+        print('.'*52)
 
         if not alphapept.interface.HEADLESS:
             input("Press Enter to continue...")
