bbe58b04c73e25394394ec6c591bf19ef37ae56c
Maximilian Strauss <straussmaximilian@gmail.com>
2022-09-13 21:41:09 +0200

    FIX

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from pkg_resources import parse_version
 from configparser import ConfigParser
 import setuptools
 assert parse_version(setuptools.__version__)>=parse_version('36.2')
-
+import os
+import re
 import alphapept as package2install
 
 # note: all settings are in settings.ini; edit there, not here
